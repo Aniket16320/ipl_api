a34829bb194da36c62b76c7d193a7276cd9a99cd
Aniket16320 <102316011+Aniket16320@users.noreply.github.com>
2023-04-09 10:15:00 +0530

    Add files via upload
    commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,jsonify,request
+import ipl
+import jugaad
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hello"
+
+@app.route('/api/teams')
+def teams():
+    teams = ipl.teamAPI()
+    return jsonify(teams)
+
+@app.route('/api/teamvteam')
+def teamvteam():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+    response = ipl.team_vs_team(team1,team2)
+    return jsonify(response)
+
+
+@app.route('/api/team-record')
+def team_record():
+    team_name = request.args.get('team')
+    response = jugaad.teamAPI(team_name)
+    return response
+
+@app.route('/api/batting-record')
+def batting_record():
+    batsman_name = request.args.get('batsman')
+    response = jugaad.batsmanAPI(batsman_name)
+    return response
+
+@app.route('/api/bowling-record')
+def bowling_record():
+    bowler_name = request.args.get('bowler')
+    response = jugaad.bowlerAPI(bowler_name)
+    return response
+
+app.run(debug=True)
